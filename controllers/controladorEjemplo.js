c5ea95dc38e155c0fbfba9cb2aafa96fe337f1d2
Chiara La Valle <chiara.lavalle@mercadolibre.com>
2019-02-09 12:49:32 -0300

    first commit

---
+++ controllers/controladorEjemplo.js
@@ -0,0 +1,38 @@
+
+//EJEMPLO 1: 
+// class ControladorEjemplo {
+//     getExample(req,res){}
+// }
+
+// const controladorEjemplo = new ControladorEjemplo()
+// module.exports = controladorEjemplo
+
+
+// OTRO ARCHIVO ROUTER
+// const archivo = require("./controllers/controladorEjemplo")
+// archivo.getExample
+
+
+
+//EJEMPLO 2: 
+// exports.getExample = (req,res) => {
+//     // ...
+// }
+// exports.getExample2 = (req,res) => {
+//     // ...
+// }
+
+
+// OTRO ARCHIVO ROUTER
+
+// const {
+//     getExample,
+//     getExample2
+// } = require('./controllers/controladorEjemplo')
+
+
+class PepeCode {
+    function name(params) {
+        
+    }
+}
